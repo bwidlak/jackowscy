9ecef78376990c626c1052516446562a0699ca03
Bartosz Widlak <bartekwidlak@bwidlak.local>
2011-10-23 14:35:59 +0200

    migration: add visible to categories

---
+++ db/migrate/20111023123022_add_visible_to_categories.rb
@@ -0,0 +1,11 @@
+class AddVisibleToCategories < ActiveRecord::Migration
+
+  def self.up
+    add_column :categories, :visible, :boolean
+  end
+  
+  def self.down
+    remove_column :categories, :visible
+  end
+
+end
