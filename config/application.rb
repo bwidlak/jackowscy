2a667a4f2218fbfef2eae21595824671035b7d70
Bartosz Widlak <bartekwidlak@bwidlak.local>
2011-10-31 21:10:15 +0000

    fucking bad compass

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Jackowscy
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.generators.stylesheet_engine = :sass
+    #config.generators.stylesheet_engine = :sass
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
