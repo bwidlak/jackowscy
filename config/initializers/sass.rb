ba4c387c4f9c02539189cf3cf3a4fdbee40751e3
Bartosz Widlak <bartekwidlak@bwidlak.local>
2011-10-31 21:23:00 +0000

    fuuuucccckkkkkk

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -2,10 +2,3 @@ Sass::Engine::DEFAULT_OPTIONS[:load_paths].tap do |load_paths|
   load_paths << "#{Rails.root}/app/assets/stylesheets"
   load_paths << "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/compass/stylesheets"
 end
-
-if Rails.configuration.respond_to?(:sass)
-  Rails.configuration.sass.tap do |config|
-    # Prefer SASS, 'cause that's what real men use :)
-    config.preferred_syntax = :sass
-  end
-end
