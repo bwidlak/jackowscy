fec10edefd131939549c08f4ccb66d5a65aa3e3c
Bartosz Widlak <bartekwidlak@bwidlak.local>
2011-10-31 20:56:57 +0000

    changes to newer ver of compass

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Jackowscy
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.sass.preferred_syntax = :sass
+    config.generators.stylesheet_engine = :sass
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
