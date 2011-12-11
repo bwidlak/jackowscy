7acde64650aebe551d2789ebd498b02143d3d012
Bartosz Widlak <bartekwidlak@bwidlak.local>
2011-12-11 14:33:23 +0000

    add paggination over author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,7 +2,7 @@ class AuthorsController < ApplicationController
 
   def show
     @author = Author.find(params[:id])
-    @author_projects = @author.projects
+    @author_projects = @author.projects.paginate(:page => params[:page], :per_page => 11)
   end
 
 end
