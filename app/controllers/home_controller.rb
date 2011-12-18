45cdadfd4ba94f02b9b8e7f90ee452e41b520088
Bartosz Widlak <bartekwidlak@bwidlak.local>
2011-12-18 23:47:14 +0000

    random revert

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def show
-    @visible_projects = Project.paginate(:page => params[:page], :conditions => { :status => true, :home => true }, :per_page => 12, :order => 'RAND()')
+    @visible_projects = Project.paginate(:page => params[:page], :conditions => { :status => true, :home => true }, :per_page => 12)
   end
 
 end
